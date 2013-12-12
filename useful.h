cc63514c381d070306c99dd4643ca2c2cf42e7e9
shinyquagsire23 <mtinc2@gmail.com>
2013-12-12 15:39:56 -0800

    Forgot this too.

---
+++ useful.h
@@ -0,0 +1,35 @@
+//
+//Use only cross-engine compatible structs and functions in here!
+//Engine specific things go in useful-[GAMECODE].h!!
+//
+
+typedef struct
+{
+	u16 x, y;
+} Position;
+
+typedef struct
+{
+	u8 y, flags, x, msbXFlags;
+	u16 tileProPal, irrelivant;
+} Oam;
+
+typedef struct
+{
+	Oam oam;
+	u32 animation;
+	u8 fC, fD, fE, fF;
+	u32 f10, objectTemplate, f18, callback;
+	Position pos1, pos2;
+	u8 pos3x, pos3y, animNr, animFrame, time, f2D;
+	Position iPos, iVel;
+	u32 derp1, derp2;
+	u8 f42, f43, end;
+} OamThingy;
+
+typedef struct
+{
+	u16 f0, f1;
+	u32 oam, animation, gfx, animation2, callback;
+	u8 end;
+} ObjTemplate;
